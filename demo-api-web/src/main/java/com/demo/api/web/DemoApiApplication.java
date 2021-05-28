02510f7775bb576a5f8cc5f98b4f919c6a9504d7
VinceYuan <vinsyuan7788@gmail.com>
2021-05-28 14:23:18 +0800

    Basic setup for further integration

---
+++ demo-api-web/src/main/java/com/demo/api/web/DemoApiApplication.java
@@ -0,0 +1,20 @@
+package com.demo.api.web;
+
+import com.demo.base.common.utils.LogUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Vince Yuan
+ * @date 12/29/2020
+ */
+@Slf4j
+@SpringBootApplication(scanBasePackages = { "com.demo.*" })
+public class DemoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApiApplication.class, args);
+        log.info(LogUtils.getLogMessage("API application is started"));
+    }
+}
